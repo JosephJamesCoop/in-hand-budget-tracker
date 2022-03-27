96fd42f289a468a9360fd06939aee7f8872c23b8
JosephJamesCoop <cooper_3869@live.com>
2022-03-27 12:57:56 -0700

    cache files defined

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,11 @@
-// this will be the service worker code.
+const APP_PREFIX = 'Budgeter-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+  "./index.html", 
+  "./css/style.css",
+  "./js/idb.js",
+  "./js/indexjs"
+];
+
+
